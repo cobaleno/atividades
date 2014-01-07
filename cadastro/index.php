a351392e7159244729b62b55adc0886117ef0a02
cobaleno <dianluca@advir.com>
2014-01-07 18:37:19 -0200

    Create index.php

---
+++ cadastro/index.php
@@ -0,0 +1,20 @@
+<?php
+    require_once("menu.php");    
+
+    session_start();
+    
+    if(isset($_SESSION["cadastros"])){
+        
+        $cadastros = $_SESSION["cadastros"];
+        
+        foreach($cadastros as $pessoa) {
+            if($pessoa != null){
+                echo "$pessoa <br/>";
+            }
+        }
+    }
+    else{
+            echo "Não Existem Pessoas Cadastradas";
+    }
+
+?>
