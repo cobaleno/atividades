8d00b54a571fea0301e65e21f5ad1b8f6db94415
cobaleno <dianluca@advir.com>
2014-01-13 18:48:49 -0200

    Create menu.php

---
+++ avaliacao3/menu.php
@@ -0,0 +1,9 @@
+<a href="index.php">Listar</a>
+&nbsp;&nbsp;
+<a href="form_cadastrar.php">Cadastrar</a>
+&nbsp;&nbsp;
+<a href="form_remover.php">Remover</a>
+&nbsp;&nbsp;
+<a href="form_editar.php">Editar</a>
+&nbsp;&nbsp;
+<br/><br/>
