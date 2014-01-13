71debad173c615dae9071aab6ead57b2ff489097
cobaleno <dianluca@advir.com>
2014-01-13 18:47:05 -0200

    Create campos_pessoa.php

---
+++ avaliacao3/campos_pessoa.php
@@ -0,0 +1,18 @@
+<label>Nome do Produto: <br/></label><input type="text" name="nomeProduto" />
+            <br/><br/>
+    <input type="checkbox" name="produtoNovo" value="sim" />
+<label>Produto Novo: <br/></label>
+            <br/>
+    <input type="radio" name="cor" value="amarelo" checked="checked" /><label>Amarelo</label>
+    <input type="radio" name="cor" value="verde" /><label></label>Verde</label>
+    <input type="radio" name="cor" value="vermelho" /><label>Vermelho</label>
+            <br/>
+<label>Prazo de Entrega: <br/></label>
+    <select name="prazo">
+                <option>5 Dias</option>
+                <option>10 Dias</option>
+                <option>15 Dias</option>
+        
+    </select>
+            <br/>
+<label>Detalhe do Produto: </label><br/><textarea name="detalhe" rows="6" cols="20"/></textarea>
